caac7be67e7d880f5a976feb254c40767396eaf7
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-01-14 22:46:51 +0100

    improved project

---
+++ test/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Clear dist dir first
+directory="dist"
+filename="*"
+echo "Clearing dist..."
+if find "$directory" -name "$filename" -type f; then
+    cd dist
+    rm -r *
+    cd ..
+fi
+
+python3 -m build
+
+library_name="akr_extensions_sdk"
+if pip show "$library_name" &> /dev/null; then
+    echo "Uninstalling..."
+    pip uninstall "$library_name" <<< "y"
+fi
+
+whl_filename="*.whl"
+if find "$directory" -name "$whl_filename"; then
+    echo "Installing..."
+    cd dist
+    pip install *.whl
+
+    cd ..
+else
+    echo "No *.whl file found"
+fi
