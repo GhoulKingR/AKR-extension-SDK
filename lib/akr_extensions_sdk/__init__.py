9ebe96b6da6f7882f9791ca917184a6ebf797b40
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-01-23 20:06:49 +0100

    Updated extension

--- lib/akr_extensions_sdk/__init__.py
+++ lib/akr_extensions_sdk/__init__.py
@@ -12,23 +12,6 @@ class AnimeExtension(ABC):
             if self._choice != None and self._choice > 0:
                 break
     
-    def download_to_file(self, response, file_name: str):
-        print("Download started:", file_name)
-        response.raise_for_status()
-
-        total_size = int(response.headers.get('content-length', 0))
-        block_size = 1024  # 1 Kibibyte
-
-        progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
-
-        with open(file_name, 'wb') as file:
-            for data in response.iter_content(block_size):
-                progress_bar.update(len(data))
-                file.write(data)
-
-        progress_bar.close()
-        print("Download complete!")
-    
     @abstractmethod
     def run(self, search):
         pass
